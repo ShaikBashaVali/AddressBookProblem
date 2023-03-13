5b7795d8220dd57f20c92d510a7fd46f1a20b599
BashaVali <bashavali222@gmail.com>
2023-03-13 07:23:27 +0530

    Add Contact

--- AddressBookProblem/CreateContacts.cs
+++ AddressBookProblem/CreateContacts.cs
@@ -28,5 +28,45 @@ namespace AddressBookProblems
             contacts.email = Console.ReadLine();
             Person.Add(contacts);
         }
+        public static void PrintPerson(PersonalDetails contact)
+        {
+            Console.WriteLine("First Name: " + contact.firstName);
+            Console.WriteLine("Last Name: " + contact.lastName);
+            Console.WriteLine("City Name: " + contact.city);
+            Console.WriteLine("Address : " + contact.address);
+            Console.WriteLine("State : " + contact.state);
+            Console.WriteLine("Zip Code : " + contact.zipcode);
+            Console.WriteLine("Phone Number: " + contact.phonenumber);
+            Console.WriteLine("Email Id : " + contact.email);
+            Console.WriteLine("-------------------------------------------");
+        }
+        public static void ListPeople()
+        {
+            Console.Clear();
+            if (Person.Count == 0)
+            {
+                Console.WriteLine("Your address book is empty.\n Press any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var contact in Person)
+            {
+                PrintPerson(contact);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
+        public static void DisplayHelp()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter only Available Commands are :");
+            Console.WriteLine("add => \tAdds a person to address book");
+            Console.WriteLine("edit => \tEdit a person to address book by first name");
+            Console.WriteLine("remove => \tRemoves a person from address book");
+            Console.WriteLine("list => \tLists out all people in the address book");
+            Console.WriteLine("Press \tAny key to continue");
+            Console.WriteLine("\n");
+        }
     }
 }
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -7,6 +7,30 @@ namespace AddressBookProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book");
+            string command = "";
+            while (command != "exit")
+            {
+                Console.Clear();
+                Console.WriteLine("The commands are:\n 1.add\n 2.list\n 3.exit\n ");
+                Console.WriteLine("Please enter a command: ");
+                command = Console.ReadLine().ToLower();
+
+                switch (command)
+                {
+                    case "add":
+                        CreateContacts.PersonInformations();
+                        break;
+                    case "list":
+                        CreateContacts.ListPeople();
+                        break;
+                    default:
+                        if (command != "exit")
+                        {
+                            CreateContacts.DisplayHelp();
+                        }
+                        break;
+                }
+            }
         }
     }
 }
